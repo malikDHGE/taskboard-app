1c5cca5d2c8ac2f8e230e89743d89dec260e57a9
malikDHGE <surfingminer1@gmail.com>
2026-04-21 00:27:21 +0200

    Fix context load test to use H2 in CI

--- backend/src/test/java/com/taskboard/TaskboardApplicationTests.java
+++ backend/src/test/java/com/taskboard/TaskboardApplicationTests.java
@@ -2,8 +2,16 @@ package com.taskboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb_context;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class TaskboardApplicationTests {
 
     @Test
